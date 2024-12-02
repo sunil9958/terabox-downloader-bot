a25699278305909d0b4c970b1b0d549b414e94ba
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-12-02 23:59:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
+HOST = "redis://default:XKtuFZv4M1mGq8hit0CGpbLgjN1YI9id@redis-18240.c275.us-east-1-4.ec2.redns.redis-cloud.com"  # redis host uri
+PORT = 18240  # redis port
 PASSWORD = ""  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
