414c76e6a4d7d7dacaa0aac6963890b0a53c2854
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-12-03 00:08:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = 25637343  # api id
+API_HASH = "70fb79a89ec2d30cab05704e817e5be6"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7339282485:AAEVPcHOC-4isReGanEpue9Nu3KkTeTJsQw"  # bot token
 
 
 ## REDIS
@@ -9,6 +9,6 @@ HOST = "redis://default:XKtuFZv4M1mGq8hit0CGpbLgjN1YI9id@redis-18240.c275.us-eas
 PORT = 18240  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002116587982  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+ADMINS = [5390137933]
